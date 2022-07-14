0e6c00d357b9f9f9945c5db4558767796bd55e96
Ugochukwu Miracle <77087947+skybaba123@users.noreply.github.com>
2022-07-14 22:22:10 +0100

    updated script

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function multiply(a, b){
 return a * b
 }
 
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+function XO(str) {
+  const string = str.split('');
+  let occurx = 0;
+  let occuro = 0;
+  string.forEach(element => {
+    if (element === 'o' || element === 'O') {
+      return `${element}: ${occuro++}`;
+    }
+    if (element === 'x' || element === 'X') {
+      return `${element}: ${occurx++}`;
+    }
+  });
+  if (occuro === occurx) {
+    return true;
+  } else {
+    return false;
+  }
+}
