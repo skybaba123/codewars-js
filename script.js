31e3761cc7c692bc37491c11c542111e00ce2305
Ugochukwu Miracle <77087947+skybaba123@users.noreply.github.com>
2022-07-15 23:23:46 +0100

    updated

--- script.js
+++ script.js
@@ -27,3 +27,8 @@ function XO(str) {
 function findShort(s){
   return s.split(' ').map(s => s.length).reduce((a, b) => Math.min(a, b));  
 }
+
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+function filter_list(l) {
+  return l.filter(n => typeof n === 'number');
+}
