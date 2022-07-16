a2e8a80c3148cc67568a90d824af24b71d7057c1
Ugochukwu Miracle <77087947+skybaba123@users.noreply.github.com>
2022-07-16 11:41:17 +0100

    updated scripts

--- script.js
+++ script.js
@@ -35,4 +35,6 @@ function filter_list(l) {
 
 //Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 //It should remove all values from list a, which are present in list b keeping their order.
-
+function arrayDiff(a, b) {
+  return [...a].filter((a) => ![...b].includes(a));
+}
